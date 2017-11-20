18d4b037e3fcda0e552b0f70d07ece70ea94923d
Christof Stocker <stocker.christof@gmail.com>
2017-11-21 00:24:27 +0100

    small changes to SGFNode

--- src/types.jl
+++ src/types.jl
@@ -1,6 +1,11 @@
 struct SGFNode
-    properties::Dict{Symbol,Any}
+    properties::Dict{Symbol,Vector{Any}}
 end
+
+SGFNode() = SGFNode(Dict{Symbol,Vector{Any}}())
+SGFNode(pairs::Pair{Symbol,<:Vector}...) = SGFNode(Dict{Symbol,Vector{Any}}(pairs...))
+SGFNode(pair::Pair{Symbol,<:Vector}) = SGFNode(Dict{Symbol,Vector{Any}}(pair))
+SGFNode(pair::Pair{Symbol,<:Any}) = SGFNode(pair.first => [pair.second])
 
 for fun in (:haskey, :getindex, :getkey, :get, :keys, :values,
             :keytype, :valtype)
@@ -11,7 +16,6 @@ end
 function Base.show(io::IO, n::SGFNode)
     if haskey(io, :compact)
         for (i,p) in enumerate(n.properties)
-            join(map(v->string('[',v,']'), p.second))
             val = if length(p.second) == 1
                 s = first(p.second)
                 if length(s) > 2
@@ -30,8 +34,15 @@ function Base.show(io::IO, n::SGFNode)
             i < length(n.properties) && print(io, " ")
         end
     else
-        print(io, typeof(n), " using a ")
-        show(io, MIME"text/plain"(), n.properties)
+        print(io, typeof(n))
+        # a little hack to reuse the Dict show method
+        tio = IOBuffer()
+        show(tio, MIME"text/plain"(), n.properties)
+        str = readstring(seek(tio,0))
+        str = replace(str, "Dict{Symbol,Array{Any,1}}", "")
+        str = replace(str, "entry", "property")
+        str = replace(str, "entries", "properties")
+        print(io, str)
     end
 end
 
